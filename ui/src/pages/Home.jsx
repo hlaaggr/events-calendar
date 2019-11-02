fd7374ec10e22ba085bea35a669cfd9116db7ce8
jane wie <janewie@gmail.com>
2019-11-02 11:38:58 -0400

    remove config dir

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 import { EventsCalendar } from '../components/EventsCalendar'
 
 
+const CALENDAR_ID = 'hlaa-calendar-app-service@hlaa-calendar-257804.iam.gserviceaccount.com';
+const API_KEY = '5681624bc3c392fca498039c0d35c4a07fc6d46e' // need to check this key, doesnt seem to be working
+const hlaaCalendarUrl = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`
+
 const Home = () => {
   return (
     <div>
