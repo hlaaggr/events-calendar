0e42fb6269c8273cf2bb455bc076c841e4d984cf
jane wie <janewie@gmail.com>
2019-11-02 12:27:04 -0400

    get hlaa calendar events to show

--- ui/src/pages/Home.jsx
+++ ui/src/pages/Home.jsx
@@ -2,18 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { EventsCalendar } from '../components/EventsCalendar'
 
-
-const CALENDAR_ID = 'hlaa-calendar-app-service@hlaa-calendar-257804.iam.gserviceaccount.com';
-const API_KEY = '5681624bc3c392fca498039c0d35c4a07fc6d46e' // need to check this key, doesnt seem to be working
+const CALENDAR_ID = 'hlaa-ggr@hearingloss-ggr.org';
+const API_KEY = 'AIzaSyCEE4HXsxiGA3uh-oD8rLo_egoENeQNmvU'
 const hlaaCalendarUrl = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`
 
-const Home = () => {
-  return (
-    <div>
-      Home Page
-      <EventsCalendar events={[]}/>
-    </div>
-  );
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      events: [],
+      errors: []
+    }
+  }
+
+  componentDidMount() {
+   this.fetchCalendarEvents();
+  }
+
+  fetchCalendarEvents() {
+    let events = [];
+    fetch(hlaaCalendarUrl)
+      .then(response => response.json())
+      .then(results => {
+        let eventsData = results.items;
+        eventsData.map((event) => {
+          events.push({
+            start: event.start.date || event.start.dateTime,
+            end: event.end.date || event.end.dateTime,
+            title: event.summary
+          })
+        });
+        this.setState({ events })
+      });
+      // error handling 
+  }
+
+
+  render() {
+    const { events } = this.state;
+    return (
+      <div>
+        Home Page
+      <EventsCalendar events={events} />
+      </div>
+    );
+  }
 };
 
 
