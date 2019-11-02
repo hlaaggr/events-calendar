ba2902c89afe57668e6592264b13492686802632
Jamie Bliss <jamie@ivyleav.es>
2019-11-02 15:04:53 -0400

    More model stuff

--- api/events/models.py
+++ api/events/models.py
@@ -4,11 +4,15 @@ from django.db import models
 class GCalEvent(models.Model):
     ...
     # GCal ID
+    # Date cache
+    # TODO: Cache refresh policies
 
 
 class RSVP(models.Model):
     event = models.ForeignKey(GCalEvent)
-    user = ...
+    user = models.ForeignKey('drf_firebase_auth.FirebaseUser')
     email = models.EmailField(required=False, blank=True)
     phone = models.CharField(required=False, blank=True)
     notified = models.Boolean(default=False)
+
+    # TODO: Send notifications
