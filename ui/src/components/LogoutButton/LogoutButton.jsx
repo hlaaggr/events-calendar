eaba36685ec0de977ddb89c58db5cdc8b9a12705
jane wie <janewie@gmail.com>
2019-11-02 21:55:43 -0400

    remove button class on nav link

--- ui/src/components/LogoutButton/LogoutButton.jsx
+++ ui/src/components/LogoutButton/LogoutButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { withFirebase } from '../../utils/Firebase';
 
 const LogoutButton = ({ firebase }) => (
-  <button onClick={firebase.doSignOut} className="navbar__link button button--secondary">
+  <button onClick={firebase.doSignOut} className="navbar__link">
     Sign Out
   </button>
 );
