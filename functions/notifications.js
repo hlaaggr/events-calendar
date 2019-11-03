cddcbaeee5cf1c9ccfae894955f1fe41214aad50
Ross-Hunter <ross-hunter@ross-hunter.com>
2019-11-03 13:31:33 -0500

    Adds additional scheduled notifications

--- functions/notifications.js
+++ functions/notifications.js
@@ -6,10 +6,19 @@ const cors = require('cors')({
 
 const templates = {
   two_days_prior(event, userProfile) {
-    return `Hi ${userProfile.email}, ${event.eventTitle} is happening on ${moment(event.event_begins).toISOString()}`;
+    return `Hi ${userProfile.email}, ${event.eventTitle} is happening in 2 day son ${moment(event.event_begins).toISOString()}`;
+  },
+  two_hours_prior(event, userProfile) {
+    return `Hi ${userProfile.email}, ${event.eventTitle} is happening in 2 hours on ${moment(event.event_begins).toISOString()}`;
   },
   event_changed(event, userProfile) {
     return `Hi ${userProfile.email}, ${event.eventTitle} has changed`;
+  },
+  post_survey(event, userProfile) {
+    return `Hi ${userProfile.email}, how was ${event.eventTitle}?`;
+  },
+  post_thanks(event, userProfile) {
+    return `Hi ${userProfile.email}, thanks for going to ${event.eventTitle}!`;
   },
 };
 
@@ -56,6 +65,42 @@ exports.twoDaysPrior = () => {
     .then((events) => {
       events.forEach((event) => {
         notifyEvent(event, 'two_days_prior');
+      });
+    });
+};
+
+exports.twoHoursPrior = () => {
+  const start = moment().add(2, 'hours').startOf('hour').toDate();
+  const end = moment().add(2, 'hours').endOf('hour').toDate();
+
+  return getEventsInRange(start, end)
+    .then((events) => {
+      events.forEach((event) => {
+        notifyEvent(event, 'two_hours_prior');
+      });
+    });
+};
+
+exports.postSurvey = () => {
+  const start = moment().subtract(1, 'day').startOf('day').toDate();
+  const end = moment().subtract(1, 'day').endOf('day').toDate();
+
+  return getEventsInRange(start, end)
+    .then((events) => {
+      events.forEach((event) => {
+        notifyEvent(event, 'post_survey');
+      });
+    });
+};
+
+exports.postSurvey = () => {
+  const start = moment().subtract(2, 'days').startOf('day').toDate();
+  const end = moment().subtract(2, 'days').endOf('day').toDate();
+
+  return getEventsInRange(start, end)
+    .then((events) => {
+      events.forEach((event) => {
+        notifyEvent(event, 'post_thanks');
       });
     });
 };
